45fc25fa29e3be64082f109586cc01f724c637c0
Test <test@tests-mac.local>
2018-12-05 16:25:54 -0800

    Initial Commit

---
+++ Swift Fin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fin
+//
+//  Created by Test on 2018-12-05.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
